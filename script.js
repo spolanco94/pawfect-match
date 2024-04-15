99dbcf082c1be808734b5eee270302dfed7e9a27
spolanco94 <6752411+spolanco94@users.noreply.github.com>
2024-04-15 19:46:59 -0400

    Update header and navigation styling
    - Improve menu styling for devices between 600px and 769px
    - Redesign navigation for devices smaller than 600px to be single column
    - Create script to reorder links and hamburger/chevron toggle for visual clarity

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 document.querySelector('.hamburger').addEventListener('click', function() {
-    document.querySelector('.nav-links').classList.toggle('show');
+    var navBar = document.querySelector('#nav-bar');
+    var navLinks = document.querySelector('.nav-links');
+    var hamburger = this;
+
+    navLinks.classList.toggle('show');
+    hamburger.classList.toggle('active');
+
+    if (navLinks.classList.contains('show')) {
+        navBar.appendChild(hamburger);
+    } else {
+        navBar.insertBefore(hamburger, navLinks);
+    }
+
+    console.log('clicked'+document.querySelector('.nav-links').classList);
 });
