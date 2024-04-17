a7f82bc7c9965495a1ed5cf5c9ba41787ff97ac2
spolanco94 <6752411+spolanco94@users.noreply.github.com>
2024-04-17 17:58:31 -0400

    Create a home link from logo and company name
    - Rework navbar to adjust for size changes with a tag
    - Fix issue where menu items did not stack properly on devices below
      369px width
    - Remove white space

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ document.querySelector('.hamburger').addEventListener('click', function() {
     var navBar = document.querySelector('#nav-bar');
     var navLinks = document.querySelector('.nav-links');
     var hamburger = this;
+    var logoName = document.querySelector('.logo-name');
 
+    navBar.classList.toggle('menu-open');
     navLinks.classList.toggle('show');
     hamburger.classList.toggle('active');
 
     if (navLinks.classList.contains('show')) {
         navBar.appendChild(hamburger);
+        logoName.style.flexDirection = 'column';
     } else {
         navBar.insertBefore(hamburger, navLinks);
+        logoName.style.flexDirection = 'row';
     }
 
     console.log('clicked'+document.querySelector('.nav-links').classList);
